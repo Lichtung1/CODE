3e513405483b86965ad5fc1dacf249e6ba3720a5
Lichtung1 <50086218+Lichtung1@users.noreply.github.com>
2023-09-12 11:54:12 -0500

    Update ode_solver_streamlit.py

--- ode_solver_streamlit.py
+++ ode_solver_streamlit.py
@@ -35,9 +35,11 @@ def ode_solver(equation_str, X_1, X_2, *initial_conditions):
             ax.annotate(label, xy=(x_vals[-1], general_solution.rhs.subs(x, x_vals[0])), fontsize=10)
             general_solution_text_str += f"\n{label}: {general_solution.rhs}"
 
+        label = 'Particular Solution'
         ax.plot(x_vals, [particular_solution.rhs.subs(x, val_x) for val_x in x_vals], 'r-', label = 'Particular Solution')
-        
+        particular_solution_text_str = f"{label}: {particular_solution.rhs}"    
         ax.legend()
+        
         ax.set_title("1st Order ODE Solutions")
 
     elif "C1" in str(sol) and "C2" in str(sol):  # 2nd order
