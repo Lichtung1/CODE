f40390a24f8a54af2463319228793c3048daae0f
Lichtung1 <50086218+Lichtung1@users.noreply.github.com>
2024-05-04 00:57:57 -0500

    Update janssen_streamlite.py

--- janssen_streamlite.py
+++ janssen_streamlite.py
@@ -22,7 +22,7 @@ def plot_janssen_equation(rho, g, R, k, mu, h_max):
 st.title("Janssen Equation Plotter")
 
 rho = st.number_input("Density (kg/m^3):", value=755.0)
-g = st.number_input("Acceleration due to gravity (m/s^2):", value=9.81)
+#g = st.number_input("Acceleration due to gravity (m/s^2):", value=9.81)
 R = st.number_input("Bin radius (m):", value=2.3)
 k = st.number_input("Janssen coefficient:", value=0.4)
 mu = st.number_input("Coefficient of friction:", value=0.4)
